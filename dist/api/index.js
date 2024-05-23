d64e43ecdf54bbd158ff2a3d69d5bea08de90fa0
Alan Killip <alankillip@hotmail.com>
2024-05-23 15:26:45 +0100

    first commit

--- dist/api/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GET = void 0;
-function GET() {
-    return "hello from vercel functions";
-}
-exports.GET = GET;
-//# sourceMappingURL=index.js.map
