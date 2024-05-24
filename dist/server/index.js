c71cff6672a29b42a7f0af2ed9b9e353e285245c
Alan Killip <alankillip@hotmail.com>
2024-05-24 09:07:36 +0100

    env example

--- dist/server/index.js
+++ dist/server/index.js
@@ -11,11 +11,29 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const tailwindService_1 = require("../services/tailwindService");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+console.log("process.env.ALLOWED_ORIGINS", process.env.ALLOWED_ORIGINS);
+const allowedOrigins = ((_a = process.env.ALLOWED_ORIGINS) === null || _a === void 0 ? void 0 : _a.split(",")) || [];
+app.use((0, cors_1.default)({
+    origin: function (origin, callback) {
+        if (!origin)
+            return callback(null, true);
+        console.log("allowedOrigins = ", allowedOrigins);
+        console.log("origin = ", origin);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+            console.error(msg);
+            return callback(null, false);
+        }
+        return callback(null, true);
+    },
+}));
 const port = process.env.PORT;
 app.get("/", (_req, res) => {
     res.send("Stylelab tailwind service");
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,31 @@
 import express, { Request, Response } from "express";
 import { getTailwindCSSFromUtilClasses } from "../services/tailwindService";
+import cors from "cors";
 
 const app = express();
+
 app.use(express.json());
+
+console.log("process.env.ALLOWED_ORIGINS", process.env.ALLOWED_ORIGINS);
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      console.log("allowedOrigins = ", allowedOrigins);
+      console.log("origin = ", origin);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        console.error(msg);
+        return callback(null, false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
 const port = process.env.PORT;
 
 app.get("/", (_req: Request, res: Response) => {
